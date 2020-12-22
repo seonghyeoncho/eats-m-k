91261de231df632a27b8974c5e316d8d4db94a48
조성현 <joseonghyeon@joseonghyeon-ui-MacBookPro.local>
2020-12-23 01:54:57 +0900

    20.12.13

--- eats-m-a/src/component/App.tsx
+++ eats-m-a/src/component/App.tsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+import {dbService} from '../firebase';
+import Order from '../component/Order';
+
+
+const App = () => {
+
+  const [ table,setTable ] =useState<any>([]);
+  const [ store,setStore ] = useState<string>("멘동");
+  //const [ order, setOrder ] = useState<any>([]);
+  
+
+  
+ 
+  
+  useEffect(()=>{
+    
+
+   dbService.collection(`${store}`).onSnapshot((snapshot:any)=>{
+      setTable([]);
+      snapshot.docs.map((doc:any)=>{
+        //console.log(doc.data().test, doc.id);
+        const tableObj = {
+          table:doc.id,
+          orders:doc.data().test
+          
+        }
+        setTable((prev:any)=>[tableObj, ...prev]);
+      });
+    })
+
+   
+
+  },[]);
+
+  console.log(table);
+  
+  
   return (
     <div className="App">
       <div>
         <h1>EATSROAD ADMIN</h1>
-        <h1>STORE : {}</h1>
+        <h1>STORE : {store}</h1>
       </div>
 
       <div>
         <h1>주문 보드</h1>
-        <div>
-          <button>처리완료</button>
-        </div>
+        {table.map((m:any)=>
+          <>
+            <h1 key={m.table}>{m.table}</h1>
+            <Order orders={m.orders}/>
+            
+          </>
+
+        )}
+
+       
       </div>
-      <div>
-        <h1>주문 완료 보드</h1>
-      </div>
+     
       
     </div>
   );
---
+++ eats-m-a/src/component/Order.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+
+
+const Order = (orders:any):any =>{
+
+    const order:any[] = [];
+  
+    const filterData = () =>{
+  
+      for(let i in orders){
+        for(let k in orders[i]){
+          order.push(orders[i][k]);
+        }
+      }
+  
+    }
+    
+    filterData();
+    console.log(order);
+  
+    
+  
+    return(
+      <div>
+        {order.map((m:any)=>
+          <>
+            <div key={m.menu}>{m.menu}{m.n}</div>
+          </>
+        )}
+  
+      </div>
+    );
+  
+  }
+
+export default Order;

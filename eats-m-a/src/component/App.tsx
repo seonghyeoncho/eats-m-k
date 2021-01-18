ecf1dd291d45b66d15e000cbe9f81ed49ad2ee3d
quintuplets2000 <quintuplets2000@gmail.com>
2021-01-18 17:11:54 +0900

    css fixed

--- eats-m-a/src/component/App.tsx
+++ eats-m-a/src/component/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Menu, Radio} from 'antd';
+import {Button, Menu, Radio} from 'antd';
 import queryString from 'query-string';
 import {dbService} from '../firebase';
 import Order from '../component/Order';
@@ -7,6 +7,7 @@ import '../scss/App.scss';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import NewOrderList from './NewOrderList';
 import CompleteOrderList from './CompleteOrderList';
+import {LeftCircleOutlined, RightCircleOutlined} from "@ant-design/icons";
 
 
 
@@ -15,36 +16,48 @@ const App = () => {
 
   const [ table,setTable ] = useState<any>([]);
   const [ state,setState ] = useState<number>(0);
-  const [radio,setRadio]=useState<any>(1);
+  const [radio,setRadio]=useState<any>(0);
+  const [date, setDate] = useState<any>(new Date());
+  const [page, setPage]=useState<number>(1);
+
+
 
   const onChangeRadio= (e:any) => {
       console.log('radio checked', e.target.value);
       setRadio(e.target.value);
   };
 
+  useEffect(()=>{
+      let timerID =setInterval(()=>tick(),1000);
+      return function cleanUp(){
+          clearInterval(timerID);
+      };
+  });
+  function tick() {
+      let now = new Date();
+      setDate(now);
+  }
 
   useEffect(()=>{
+        dbService.collection(`${query.store}`)
+            .orderBy("orderAt")
+            .onSnapshot((snapshot: any) => {
+                setTable([]);
+                snapshot.docs.map((doc: any) => {
+                        console.log(doc.data().orderList);
 
-    dbService.collection(`${query.store}`)
-      .orderBy("orderAt")
-      .onSnapshot((snapshot:any)=>{
-        setTable([]);
-        snapshot.docs.map((doc:any)=>{
-          console.log(doc.data().orderList);
+                        const tableObj = {
 
-          const tableObj = {
-            
-            myTable:doc.id,
-            orders:doc.data().orderList,
-            check:doc.data().check
-          }
+                            myTable: doc.id,
+                            orders: doc.data().orderList,
+                            check: doc.data().check
+                        }
 
-          setTable((prev:any)=>[tableObj, ...prev]);
+                        setTable((prev: any) => [tableObj, ...prev]);
 
-        }
-      );
-    })
-
+                    }
+                );
+            })
   },[]);
 
   const toggleCheck = (t:number) => {
@@ -86,6 +99,36 @@ const App = () => {
               price:6500
             }
           ]
+      },
+      {
+          check: false,
+          myTable: "4",
+          orders: [{
+              count:2,
+              menu:"라멘",
+              price:6500
+          },
+              {
+                  count:4,
+                  menu:"가츠동",
+                  price:6500
+              }
+          ]
+      },
+      {
+          check: false,
+          myTable: "11",
+          orders: [{
+              count:1,
+              menu:"라멘",
+              price:6500
+          },
+              {
+                  count:4,
+                  menu:"돈코츠",
+                  price:7000
+              }
+          ]
       }
   ];
 console.log('더미데이터');
@@ -95,7 +138,7 @@ console.log(table);
 
   const listState = () => {
 
-      if(state === 0) return <NewOrderList table={tableDummy} toggleCheck={toggleCheck}/>
+      if(state === 0) return <NewOrderList table={tableDummy} toggleCheck={toggleCheck} indexNumber={page}/>
       else return <CompleteOrderList table={table}/>
   }
  
@@ -111,24 +154,44 @@ console.log(table);
 
       <div className="orderButtonClass">
 
-        <button className={`newOrderButton ${!state && "buttonClicked"}`} onClick={()=>setState(0)}>새로운주문</button>
+        <button className={`newOrderButton ${!state && "buttonClicked"}`} onClick={()=>{
+            setState(0);
+            setPage(1);
+        }}>새로운주문</button>
         <button className={`completeOrderButton ${state && "buttonClicked"}`} onClick={()=>setState(1)}>접수완료</button>
       </div>
+      <div className="infoBar">
+          <div className="radioDiv">
+              <Radio.Group onChange={onChangeRadio} value={radio}>
+                  <Radio value={0}>최신 주문순</Radio>
+                  <Radio value={1}>과거 주문순</Radio>
+              </Radio.Group>
+          </div>
+          <div className="timer">
+              <h1>{date.toLocaleString('kr')}</h1>
+          </div>
 
-      <div className="radioDiv">
-          <Radio.Group onChange={onChangeRadio} value={radio}>
-              <Radio value={1}>과거 주문순</Radio>
-              <Radio value={2}>최신 주문순</Radio>
-          </Radio.Group>
-          <hr/>
       </div>
-
+      <hr className="infoHr"/>
 
       <div>
           {listState()}
       </div>
+      <div className="pageButton">
+          <LeftCircleOutlined className="circleButton" onClick={()=>{
+              if(page>1){
+                  setPage(page-1);
+              }
+          }}/>
+          <h1>{page}/{table.length/3}</h1>
+          <RightCircleOutlined className="circleButton" onClick={()=>{
+              if(page<table.length/3+1){
+                  setPage(page+1);
+              }
+          }}/>
+      </div>
       <hr/>
-     
+
       
     </div>
   );
--- eats-m-a/src/component/NewOrderList.tsx
+++ eats-m-a/src/component/NewOrderList.tsx
@@ -4,12 +4,12 @@ import { Card, Col, Row,Button } from 'antd';
 import '../scss/NewOrderList.scss';
 
 interface Props {
-
     table:any
     toggleCheck:(t:number)=>void
-
+    indexNumber:number
 }
-const NewOrderList = ({table,toggleCheck}:Props) => {
+
+const NewOrderList = ({table,toggleCheck,indexNumber}:Props) => {
 
   const [select,setSelect] = useState<any>();
 
@@ -18,25 +18,38 @@ const NewOrderList = ({table,toggleCheck}:Props) => {
     }
 
   let c = 0;
-  
+  let tables=[];
+  let i=indexNumber*3-3;
+  for(i;i<indexNumber*3;i++){
+      if(table[i]!=null){
+          tables.push(table[i]);
+      }
+  }
+
   return (
 
-    <div>
-      {table.map((m:any)=>{
-          
+    <div className="row">
+      {tables.map((m:any)=>{
         if(!m.check){
-          return(
-              <Col span={8}>
-                  <Card className="orderCard" onClick={()=>onClick(m.orders)} >
-                      <h1>{`${m.myTable}번 테이블`}</h1>
-                      <hr/>
-                    <Card className="orderMeta">
-                        <Order orders={m.orders}/>
-                    </Card>
-                    <Button className="orderFinishedButton" onClick={()=>toggleCheck(m.myTable)}>주문완료</Button>
-                  </Card>
-              </Col>
-          )
+            for(let i=0; i<indexNumber*3;i++) {
+                return (
+                    <Col span={8}>
+                        <Card className="orderCard" onClick={() => onClick(m.orders)}>
+                            <h1>{`${m.myTable}번 테이블`}</h1>
+                            <hr/>
+                            <Order orders={m.orders}/>
+                            <Button className="orderFinishedButton" onClick={() => toggleCheck(m.myTable)}><h1>주문완료</h1>
+                            </Button>
+                        </Card>
+                    </Col>
+                )
+            }
+        }else{
+            return(
+                <div>
+                    <h1>새로운 주문이 없습니다</h1>
+                </div>
+            )
         }
             
 

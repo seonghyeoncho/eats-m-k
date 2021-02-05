6d496e1585f65aab1ea9fdd463623e95b5eb7815
seonghyeoncho <chosh040@naver.com>
2021-02-05 14:32:25 +0900

    옵션 최대 수량 구현

--- src/component/Detail/OptoinSelect/Options.tsx
+++ src/component/Detail/OptoinSelect/Options.tsx
@@ -1,3 +1,4 @@
+import { SSL_OP_SSLEAY_080_CLIENT_DH_BUG } from 'constants';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import numberWithCommas from '../../../functions/addCommaFunc';
@@ -18,16 +19,25 @@ interface Props {
 const Options = ({ maxSelect, options, optionName, }:Props) => {
 
     const dispatch = useDispatch();
+    const [ totalCheck, setTotalCheck ] = useState<number>(0);
 
     const optionHandler = (selectOption:Option, isChecked: boolean) => {
+        console.log(isChecked);
         const option = {
             name: selectOption.name,
             price: selectOption.price,
             optionCategory: optionName
         };
         if(isChecked){
-            dispatch(SelectAction.selectOptoin(option));
+            if(totalCheck < maxSelect) {
+                setTotalCheck(totalCheck + 1);
+                dispatch(SelectAction.selectOptoin(option));
+
+            } else {
+                console.log('over max select!');
+            }
         } else {
+            setTotalCheck(totalCheck - 1);
             dispatch(SelectAction.deleteOptoin(option));
         };
     };
@@ -37,7 +47,7 @@ const Options = ({ maxSelect, options, optionName, }:Props) => {
             {
                 options.map((op:Option) => {
                     return (
-                        <SelectItem name={op.name} price={op.price} optionHandler={optionHandler}/>
+                        <SelectItem name={op.name} price={op.price} optionHandler={optionHandler} maxSelect={maxSelect} totalCheck={totalCheck}/>
                     )
                 })
             }
--- src/component/Detail/OptoinSelect/SelectItem.tsx
+++ src/component/Detail/OptoinSelect/SelectItem.tsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import numberWithCommas from '../../../functions/addCommaFunc';
 import Checked from '../../../image/icons/icon_CheckBox_selected_x3.png';
 import NotCheck from '../../../image/icons/icon_CheckBox_deselect_x3.png';
+import Rchecked from '../../../image/icons/icon_radio_selected_x3.png';
+import RNotCheck from '../../../image/icons/icon_radio_deselected_x3.png';
 
 interface Option {
     name: string,
@@ -12,20 +14,38 @@ interface Props {
     name: string;
     price: number;
     optionHandler: ( op:Option, isChecked: boolean ) => void;
+    maxSelect: number;
+    totalCheck: number;
 }
 
-const SelectItem = ({ name, price, optionHandler }:Props) => {
+const SelectItem = ({ name, price, optionHandler, maxSelect, totalCheck }:Props) => {
     const [ isChecked, setIsChecked ] = useState<boolean>(true);
     
     const option: Option = {
         name: name,
         price: price
     };
+    const checkCount = () => {
+        if(isChecked){
+            if(totalCheck< maxSelect){
+                setIsChecked(!isChecked);
+                optionHandler(option, isChecked);
+            } else {
+                console.log('sdfsdf');
+            }
+        } else {
+            setIsChecked(!isChecked);
+                optionHandler(option, isChecked);
+        }
+    }
     
     return (
-        <div className="item" onClick={() => {setIsChecked(!isChecked);optionHandler(option, isChecked);}}>
+        <div className="item" onClick={checkCount}>
             <div className="con">
-                <img className='check' src={!isChecked ? Checked:NotCheck}></img>
+                {
+                    maxSelect !== 1? <img className='check' src={!isChecked ? Checked : NotCheck} alt="check"></img>
+                    : <img className='Rcheck' src={!isChecked ? Rchecked : RNotCheck} alt="check"></img>
+                }
                 <div className="name">{name}</div>
             </div>
             

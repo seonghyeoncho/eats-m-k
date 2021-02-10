16dd62574d3da084d3032bc363868465c0650071
seonghyeoncho <chosh040@naver.com>
2021-02-10 20:14:31 +0900

    loading

--- src/component/AddMenuButton/GlobalLoading.tsx
+++ src/component/AddMenuButton/GlobalLoading.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import loadingGIf from '../../image/gifs/graphic_loading.gif'
+import './GlobalLoading.scss';
 
 const GlobalLoading = () => {
     return (
-        <div>
-            Loading...
+        <div className="loading-con">
+            <div className='loading-img'>
+                <img src={loadingGIf} alt='loading-img'/>
+            </div>
         </div>
     );
 };
